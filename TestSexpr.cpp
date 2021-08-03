65d2774d66ec2322c62a5b17b0333686fc771a5f
Nick Porcino <meshula@hotmail.com>
2021-08-02 22:36:28 -0700

    Much fancier test

--- TestSexpr.cpp
+++ TestSexpr.cpp
@@ -7,6 +7,65 @@ char const* test = R"(
 (+ 123.4 (* 30 45)) (elephant banana canary)
 (print "hello world")
 (hello-world)
+
+(LabSoundGraphToy
+
+    :name "SN76477"
+
+    (ls-node :name "Gain-3" :kind "Gain" :pos 869 116
+        (pins
+            '(:name "gain" :kind "param" :value 1.0))) 
+    (ls-node :name "Gain-2" :kind "Gain" :pos 871 332
+        (pins
+            '(:name "gain" :kind "param" :value 1.0))) 
+    (ls-node :name "Gain-1" :kind "Gain" :pos 1140 260
+        (pins
+            '(:name "gain" :kind "param" :value 1.0))) 
+    (ls-node :name "Oscillator-2" :kind "Oscillator" :pos 56 170
+        (pins
+            '(:name "type" :kind "setting" :value "Enumeration Sine")
+            '(:name "frequency" :kind "param" :value 2.0)
+            '(:name "amplitude" :kind "param" :value 10.0)
+            '(:name "bias" :kind "param" :value 0.0)
+            '(:name "detune" :kind "param" :value 0.0)))
+    (ls-node :name "Oscillator-1" :kind "Oscillator" :pos 450 110
+        (pins
+            '(:name "type" :kind "setting" :value "Enumeration Sine")
+            '(:name "frequency" :kind "param" :value 440.0)
+            '(:name "amplitude" :kind "param" :value 1.0)
+            '(:name "bias" :kind "param" :value 0.0)
+            '(:name "detune" :kind "param" :value 0.0)))
+    (ls-node :name "Device-1" :kind "Device" :pos 1477 263)
+    (ls-node :name "Group-1" :kind "Group" :pos 31 33)
+    (ls-node :name "BiquadFilter-1" :kind "BiquadFilter" :pos 466 330
+        (pins
+            '(:name "type" :kind "setting" :value "Enumeration Low Pass")
+            '(:name "frequency" :kind "param" :value 350.0)
+            '(:name "Q" :kind "param" :value 1.0)
+            '(:name "gain" :kind "param" :value 0.0)
+            '(:name "detune" :kind "param" :value 0.0)))
+    (ls-node :name "Noise-1" :kind "Noise" :pos 60 330
+        (pins '(:name "type" :kind "setting" :value "Enumeration White")))
+    (ls-node :name "ADSR-1" :kind "ADSR" :pos 1069 510
+        (pins
+            '(:name "type" :kind "attackTime" :value 0.125)
+            '(:name "type" :kind "attackLevel" :value 1.0)
+            '(:name "type" :kind "decayTime" :value 0.125)
+            '(:name "type" :kind "sustainLevel" :value 0.5)
+            '(:name "type" :kind "ReleaseTime" :value 0.125)
+  
+    (ls-connection :from "Gain-2" "out" :to "Gain-1" "Gain-1")
+    (ls-connection :from "Gain-3" "out" :to "Gain-1" "Gain-1")
+    (ls-connection :from "Oscillator-1" "out" :to "Gain-3" "Gain-3")
+    (ls-connection :from "Gain-1" "out" :to "Device-1" "Device-1")
+    (ls-connection :from "Gain-2" "out" :to "ADSR-1" "ADSR-1")
+    (ls-connection :from "BiquadFilter-1" "out" :to "Gain-2" "Gain-2")
+    (ls-connection :from "Oscillator-2" "out" :to "Oscillator-1" "Oscillator-1")
+    (ls-connection :from "Noise-1" "out" :to "BiquadFilter-1" "BiquadFilter-1")
+    (ls-connection :from "ADSR-2" "out" :to "Gain-1" "Gain-1")
+)
+
+
 )";
 
 
